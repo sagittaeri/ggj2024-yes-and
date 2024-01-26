bcd7bd8d6ad6448fc5adcf4593a9a33b9dc79003
Stewart G <grant-stewart@hotmail.com>
2024-01-27 10:38:54 +1100

    Ragdoll Launching and Basic Camera follow

--- Assets/_GameAssets/Code/Scripts/RigidBody Forces/EnableRagdoll.cs
+++ Assets/_GameAssets/Code/Scripts/RigidBody Forces/EnableRagdoll.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,27 @@ public class EnableRagdoll : MonoBehaviour
 {
     [SerializeField] private bool _ragDollActive = false;
 
-    [SerializeField] private Rigidbody _rb => GetComponent<Rigidbody>();
+    private Rigidbody _rb => GetComponent<Rigidbody>();
+    [SerializeField] private float _launchStrength = 100f;
+    [SerializeField] private Rigidbody _ragdollTorso;
+
+    private void Awake()
+    {
+        if (!_ragdollTorso)
+            Debug.LogError("Torso Ragdoll Unassigned.");
+    }
 
     void Update()
     {
-        if (_ragDollActive)
+        if (_ragDollActive && _ragdollTorso)
         {
             foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
             {
                 _ragDollActive = false;
                 _rb.isKinematic = true;
                 _rb.useGravity = false;
+                
+                _ragdollTorso.AddForce((transform.forward + transform.up) * _launchStrength,ForceMode.VelocityChange);
                 
                 rb.isKinematic = false;
                 rb.useGravity = true;

c597099cb011ee8887a9bda9ecf3d4f3df35830e
Stewart G <grant-stewart@hotmail.com>
2024-01-27 15:42:35 +1100

    Enum script. sorry to commit into master.

--- Assets/_GameAssets/Code/Scripts/GameController.cs
+++ Assets/_GameAssets/Code/Scripts/GameController.cs
@@ -26,33 +26,4 @@ public class GameController : MonoBehaviour
 
 [System.Serializable]
 public class FuckwitCatalogue : UnitySerializedDictionary<FuckWits, GameObject> { }
-public abstract class UnitySerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
-{
-    [SerializeField, HideInInspector]
-    private List<TKey> keyData = new List<TKey>();
-	
-    [SerializeField, HideInInspector]
-    private List<TValue> valueData = new List<TValue>();
 
-    void ISerializationCallbackReceiver.OnAfterDeserialize()
-    {
-        this.Clear();
-        for (int i = 0; i < this.keyData.Count && i < this.valueData.Count; i++)
-        {
-            this[this.keyData[i]] = this.valueData[i];
-        }
-    }
-
-    void ISerializationCallbackReceiver.OnBeforeSerialize()
-    {
-        this.keyData.Clear();
-        this.valueData.Clear();
-
-        foreach (var item in this)
-        {
-            this.keyData.Add(item.Key);
-            this.valueData.Add(item.Value);
-        }
-    }
-}
-public enum FuckWits{Gump, Scum, Xlon}
